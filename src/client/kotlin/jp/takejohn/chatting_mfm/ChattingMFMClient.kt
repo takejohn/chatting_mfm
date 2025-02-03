6d959e20305dcd476df7e5c9842864bb0d2b9e7a
takejohn <takejohn@takejohn.jp>
2025-02-03 23:18:09 +0900

    Initial commit

---
+++ src/client/kotlin/jp/takejohn/chatting_mfm/ChattingMFMClient.kt
@@ -0,0 +1,9 @@
+package jp.takejohn.chatting_mfm
+
+import net.fabricmc.api.ClientModInitializer
+
+object ChattingMFMClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
