6415068febc06c98aa1b5df1550d2b1f212b09e9
takejohn <takejohn@takejohn.jp>
2025-02-04 23:32:58 +0900

    太字、斜体、打ち消し線の描画

--- src/client/kotlin/jp/takejohn/chatting_mfm/text/MfmDrawer.kt
+++ src/client/kotlin/jp/takejohn/chatting_mfm/text/MfmDrawer.kt
@@ -1,7 +1,6 @@
 package jp.takejohn.chatting_mfm.text
 
-import com.github.samunohito.mfm.api.node.IMfmNode
-import com.github.samunohito.mfm.api.node.MfmText
+import com.github.samunohito.mfm.api.node.*
 import net.minecraft.client.font.TextRenderer
 import net.minecraft.client.font.TextRenderer.Drawer
 import net.minecraft.text.Style
@@ -20,16 +19,26 @@ object MfmDrawer {
 
     private fun drawNode(drawer: TextRenderer.Drawer, index: MutableInt, style: Style, node: IMfmNode): Boolean {
         return when (node) {
-            is MfmText -> drawText(drawer, index, style, node)
-            else -> drawString(drawer, index, style, node.stringify())
+            is MfmText -> drawText(drawer, index, style, node.props.text)
+            is MfmBold -> draw(drawer, index, style.withBold(true), node.children)
+            is MfmItalic -> draw(drawer, index, style.withItalic(true), node.children)
+            is MfmStrike -> draw(drawer, index, style.withStrikethrough(true), node.children)
+            is MfmQuote -> drawQuote(drawer, index, style, node)
+            else -> drawText(drawer, index, style, node.stringify())
         }
     }
 
-    private fun drawText(drawer: Drawer, index: MutableInt, style: Style, node: MfmText): Boolean {
-        return drawString(drawer, index, style, node.props.text)
+    private fun drawQuote(drawer: Drawer, index: MutableInt, style: Style, node: MfmQuote): Boolean {
+        // "<PlayerName> message" が "<", "PlayerName", "> message" に分割されるため、
+        // 引用構文を意図していないものが入る可能性がある。
+        // そのため、"> "を描画しておく。
+        if (!drawText(drawer, index, style, "> ")) {
+            return false
+        }
+        return draw(drawer, index, style, node.children)
     }
 
-    private fun drawString(drawer: Drawer, index: MutableInt, style: Style, string: String): Boolean {
+    private fun drawText(drawer: Drawer, index: MutableInt, style: Style, string: String): Boolean {
         val codePoints = string.codePoints().iterator()
         while (codePoints.hasNext()) {
             val codePoint = codePoints.next()
